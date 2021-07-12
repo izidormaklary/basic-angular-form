06a69cdaad120df64622cf696c26249d8a3a2e4a
izidormaklary <maklaryizidor@gmail.com>
2021-07-12 16:51:07 +0200

    form front validated

--- basic-angular-form/src/app/app.component.ts
+++ basic-angular-form/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import {Friend} from "./friend";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  #formName="ngForm"
+  languages = ["html", "css", "javascript", "php", "java", "c#"];
+  emailRegX = "^([a-zA-Z0-9_-.]+)@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.)|(([a-zA-Z0-9-]+.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$";
+  phoneRegX = "^\ (((+|0\ 0)\ ([0-9]\ [0-9]))|[0-9])(\ [0-9]){9}\ $";
   title = 'basic-angular-form';
+  friendModel = new Friend();
 }
---
+++ basic-angular-form/src/app/friend.ts
@@ -0,0 +1,9 @@
+export class Friend {
+  firstname : string = "";
+  lastname : string = "";
+  email : string ="";
+  phone : string = "";
+  language : string= "";
+constructor() {
+}
+}
