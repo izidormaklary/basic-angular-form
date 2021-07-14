82c43cadf06949cd21590cfc79efa8b665fd0846
izidormaklary <maklaryizidor@gmail.com>
2021-07-14 09:10:04 +0200

    must haves mainly done

--- basic-angular-form/src/app/app.component.ts
+++ basic-angular-form/src/app/app.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Friend} from "./friend";
+import {AddFriendService} from "./add-friend.service";
+import {combineAll} from "rxjs/operators";
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  #formName="ngForm"
+export class AppComponent implements OnInit {
+  #formName = "ngForm"
   languages = ["html", "css", "javascript", "php", "java", "c#"];
   emailRegX = "^([a-zA-Z0-9_-.]+)@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.)|(([a-zA-Z0-9-]+.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$";
   phoneRegX = "^\ (((+|0\ 0)\ ([0-9]\ [0-9]))|[0-9])(\ [0-9]){9}\ $";
   title = 'basic-angular-form';
   friendModel = new Friend();
+  allFriends: any;
+
+  constructor(private addFriendService: AddFriendService) {
+  }
+
+  ngOnInit() {
+    this.fetchFriends(this.addFriendService.url)
+  }
+
+  addFriend() {
+
+    this.addFriendService.addFriend(this.friendModel).subscribe(data => alert("s"), error => alert("it didn't work"));
+    this.fetchFriends(this.addFriendService.url);
+  }
+
+  public async fetchFriends(url : string): Promise<any> {
+    await fetch(url, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
+    }).then(response => {
+      return response.json()
+    }).then(data => (this.allFriends = data));
+
+  }
 }
--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.all("/*", function(req, res, next){
 });
 
 
-let allFriends = [{fName: 'Coach', lName: 'Tim', email: 'tim.broos@becode.org', phone: '0469420666', signatureMove: 'Yeet', language: 'Javascript'}];
+let allFriends = [{firstname: 'Coach', lastname: 'Tim', email: 'tim.broos@becode.org', phone: '0469420666', language: 'Javascript'}];
 
 // Below you can define how your API handles a get or a post request.
 // Try sending a get request to the root, you should get a "Hello from server" back.
@@ -23,10 +23,14 @@ let allFriends = [{fName: 'Coach', lName: 'Tim', email: 'tim.broos@becode.org', 
 app.get('/', function (request, response) {
     response.send('Hello from server');
 });
-
+app.get('/allFriends',function (request, response){
+    response.send(allFriends);
+});
 app.post('/', function (request, response) {
     response.status(200).send({"message": "Data received"});
 });
-
+app.post('/allFriends',function (request, response){
+    allFriends.push(request.body)
+})
 
 app.listen(PORT, function () {});
